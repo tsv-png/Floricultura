c19b438d8c66e8bbad6f269319abd3cc1bbf72b7
João Gabriel <53877116+jotagabrielp@users.noreply.github.com>
2023-04-08 13:02:31 -0300

    modifica css global e adiciona links para outras rotas

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Login from './pages/Login';
-import Adm from './pages/Adm/Adm';
-import Home from './pages/Home';
-import Cadastroadm from './pages/Cadastroadm';
-import Redefinicao from './pages/Redefinicao';
+import Login from "./pages/Login";
+import Adm from "./pages/Adm/Adm";
+import Home from "./pages/Home";
+import Cadastroadm from "./pages/Cadastroadm";
+import Redefinicao from "./pages/Redefinicao";
 
 function App() {
-  
   return (
     <Router>
       <Routes>
-        <Route exact path="/login" element={<Login/>} />
-        <Route path="/adm" element={<Adm/>} />
-        <Route path='/home' element={<Home/>} />
-        <Route path='/cadastroadm' element={<Cadastroadm/>} />
-        <Route path='/redefinicao' element={<Redefinicao/>} />
+        <Route exact path="/login" element={<Login />} />
+        <Route path="/adm" element={<Adm />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/cadastroadm" element={<Cadastroadm />} />
+        <Route path="/redefinicao" element={<Redefinicao />} />
       </Routes>
     </Router>
   );
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,37 +1,40 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import './Login.css';
-import flower from '../../assets/flower.png';
+import "./Login.css";
+import flower from "../../assets/flower.png";
 import { Link } from "react-router-dom";
-function Login (){
-    return(
-        <div className="container">
-            <div className="logo">
-                <img src={flower} alt="logo Flower Admin" />
+function Login() {
+  return (
+    <div className="container">
+      <div className="logo">
+        <img src={flower} alt="logo Flower Admin" />
+      </div>
+      <div className="login">
+        <div className="login__form">
+          <div className="form-input">
+            <div className="form-group">
+              <input type="text" className="form" placeholder="Usuário" />
             </div>
-            <div className="login">
-                <div className="login__form">
-                    
-                        <div className="form-input">
-
-                        <div className="form-group">
-                            <input type="text" className="form" placeholder="Usuário" />
-                        </div>
-                        <div className="form-group">
-                            <input type="password" className="form" placeholder="Senha" />
-                        </div>
-                        <button type="submit" className="btn-entrar">ENTRAR</button>
-                        </div>
-                     <Link className="esqueceu-senha" to='../pages/Redefinicao/Redefinicao.js'>Esqueceu a Senha?</Link>
-                   <br></br>
-                   <div className="pai">
-                     <a className="registra">Não tem uma conta?</a><Link className="registre-se" to='../pages/Cadastroadm/Cadastroadm.js'>Registre-se</Link>
-                   </div>
-                   
-                </div>
+            <div className="form-group">
+              <input type="password" className="form" placeholder="Senha" />
             </div>
-           </div>
-    );
+            <button type="submit" className="btn-entrar">
+              ENTRAR
+            </button>
+          </div>
+          <Link className="esqueceu-senha" to="/redefinicao">
+            Esqueceu a Senha?
+          </Link>
+          <br></br>
+          <div className="pai">
+            <p>Não tem uma conta?</p>
+            <Link className="registre-se" to="/cadastroadm">
+              Registre-se
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
